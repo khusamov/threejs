1d72d18e8eaa5f4dbeac3881d9fae93dee905eec
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-14 23:43:31 +0300

    Добавлено уничтожение запросов анимационных фреймов и отрисовщика

--- packages/sample1/src/components/Application/Application.tsx
+++ packages/sample1/src/components/Application/Application.tsx
@@ -1,32 +1,41 @@
 import styles from './Application.module.scss'
 import {useEffect, useRef} from 'react';
 import {BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
+const scene = new Scene
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const geometry = new BoxGeometry
+const material = new MeshBasicMaterial({color: 0x00ff00})
+const cube = new Mesh(geometry, material)
+scene.add(cube)
 
 export default function Application() {
 	const ref = useRef<HTMLDivElement>()
 
+	const frame = useRef(0)
 	useEffect(() => {
-		const scene = new Scene
-		const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 		const renderer = new WebGLRenderer
 		renderer.setSize(window.innerWidth, window.innerHeight)
 		ref.current.appendChild(renderer.domElement)
 
-		const geometry = new BoxGeometry
-		const material = new MeshBasicMaterial({color: 0x00ff00})
-		const cube = new Mesh(geometry, material)
-		scene.add(cube)
-
 		camera.position.z = 5
 
+		const orbitControls = new OrbitControls(camera, renderer.domElement)
+
 		function animate() {
-			requestAnimationFrame(animate)
-			cube.rotation.x += 0.01
-			cube.rotation.y += 0.01
+			orbitControls.update()
 			renderer.render(scene, camera)
+			frame.current = requestAnimationFrame(animate)
 		}
 
 		animate()
+
+		return () => {
+			cancelAnimationFrame(frame.current)
+			renderer.dispose()
+		}
 	}, [])
 
 	return (
