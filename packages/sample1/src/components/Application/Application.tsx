1d9d94c72421700c367c9ba9c8f8b3437409c130
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-14 23:30:12 +0300

    Добавлен пример с кубом

--- packages/sample1/src/components/Application/Application.tsx
+++ packages/sample1/src/components/Application/Application.tsx
@@ -1,8 +1,36 @@
 import styles from './Application.module.scss'
+import {useEffect, useRef} from 'react';
+import {BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from 'three';
 
 export default function Application() {
+	const ref = useRef<HTMLDivElement>()
+
+	useEffect(() => {
+		const scene = new Scene
+		const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+		const renderer = new WebGLRenderer
+		renderer.setSize(window.innerWidth, window.innerHeight)
+		ref.current.appendChild(renderer.domElement)
+
+		const geometry = new BoxGeometry
+		const material = new MeshBasicMaterial({color: 0x00ff00})
+		const cube = new Mesh(geometry, material)
+		scene.add(cube)
+
+		camera.position.z = 5
+
+		function animate() {
+			requestAnimationFrame(animate)
+			cube.rotation.x += 0.01
+			cube.rotation.y += 0.01
+			renderer.render(scene, camera)
+		}
+
+		animate()
+	}, [])
+
 	return (
-		<div className={styles.Application}>
+		<div ref={ref} className={styles.Application}>
 
 		</div>
 	)
